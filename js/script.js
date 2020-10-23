5534b2472e37935b21a653ab002fd77807f036f6
Fabrizio <whoet.wo@gmail.com>
2020-10-23 15:46:16 +0200

    Esercizio completato

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var userSurname = prompt('Inserisci il tuo cognome');
+var listSurname = ['Bianchi', 'Rossi', 'Duzioni','Balsano','Verdi'];
+var listOlSurname = document.getElementById('olSurnames');
+var listPosition = document.getElementById('olPosition');
+
+listSurname.push(userSurname);
+
+listSurname.sort();
+
+for (i = 0; i < listSurname.length; i++){
+   listOlSurname.innerHTML += '<li>' + listSurname[i] + '</li>';
+   if (userSurname == listSurname[i]){
+   listPosition.innerHTML = 'La tua posizione nella lista è ' + (i +1);
+ }
+}
